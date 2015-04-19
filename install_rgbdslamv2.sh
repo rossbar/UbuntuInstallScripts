0c7e8e38accac2b269bc1bdb825099d2b526856b
Ross Barnowski <rossbar@berkeley.edu>
2015-04-18 22:49:22 -0700

    Turned Installation section of the rgbdslam_v2 github.io page into an install script. First time it ran it had problems with rosdep but after a second run everything worked.

---
+++ install_rgbdslamv2.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Get ubuntu version
+VERSION_STRING=`lsb_release -r`
+VERSION=${VERSION_STRING//[A-Z:a]/}
+
+# Only operate on unbuntu 12.04
+if [ $VERSION == "12.04" ]; then
+  # Prepare workspace
+  mkdir -p $HOME/rgbdslam_catkin_ws/src
+  cd $HOME/rgbdslam_catkin_ws/src
+  catkin_init_workspace
+  cd $HOME/rgbdslam_catkin_ws/
+  catkin_make
+  source devel/setup.bash
+  # Get RGBDSLAM
+  cd $HOME/rgbdslam_catkin_ws/src
+  git clone https://github.com/rossbar/rgbdslam_v2.git
+  cd $HOME/rgbdslam_catkin_ws/
+  # Install
+  rosdep update
+  rosdep install rgbdslam
+  catkin_make
+  catkin_make
+  # Add devel script to bashrc
+  echo "source $HOME/rgbdslam_catkin_ws/devel/setup.bash" >> $HOME/.bashrc
+
+else
+  echo "Detected Ubuntu Version: $VERSION"
+  echo "rgbdslam_v2 autobuild only works for ubuntu 12.04"
+  echo "Exiting..."
+fi
