b8e789e0756a513b5c3bcd58fa382a195878c93b
Ross Barnowski <rossbar@berkeley.edu>
2015-06-15 09:32:04 -0700

    Added script for installing lsd_slam.

---
+++ install_lsdslam.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+### Install lsd_slam package to home directory
+WS_NAME=lsd_slam_ws
+mkdir $HOME/$WS_NAME
+cd $HOME/$WS_NAME
+rosws init . /opt/ros/indigo
+mkdir src
+rosws set $HOME/$WS_NAME/src -t .
+echo "source $HOME/$WS_NAME/setup.bash" >> $HOME/.bashrc
+source setup.bash
+cd src
+# Pull down sourcecode
+git clone https://github.com/rossbar/lsd_slam.git lsd_slam
+# Compile
+rosmake lsd_slam
