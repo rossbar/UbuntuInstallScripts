6c09c4b7889898b2d3e43f09bd4f2a0bf038e0f9
Ross Barnowski <rossbar@berkeley.edu>
2015-07-09 16:59:52 -0700

    Added -j back into make, but changed it from num_cores to num_cores - 1. Seems to have fixed the g++ bug that was encountered when all cores were used previously.

--- install_root.sh
+++ install_root.sh
@@ -5,8 +5,8 @@
 INSTALL_DIR=$HOME/InstallationFiles
 # Newest version of ROOT v5. Works in ubuntu 14.04
 ROOT_VERSION=root_v5.34.32.source.tar.gz
-#echo $ROOT_VERSION
-#NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
+NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
+USE_CPUS=`expr $NUM_CPUS - 1`
 
 # Create the installation directory, if it doesn't already exist
 mkdir -p $INSTALL_DIR
@@ -23,8 +23,8 @@ cd root
 echo "\n\nConfiguring root..."
 ./configure
 # Build
-echo "\n\nBuilding root" # on $(NUM_CPUS) cores"
-make
+echo "\n\nBuilding root on $(USE_CPUS) cores"
+make -j $USE_CPUS
 # Add root configuration script to bashrc
 echo "Adding thisroot.sh configuration to default environment..."
 echo "source $INSTALL_DIR/root/bin/thisroot.sh" >> $HOME/.bashrc
