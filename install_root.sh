60fb5d2f3a7b359c369fce94d1fd6a73813e7395
Ross Barnowski <rossbar@berkeley.edu>
2015-07-09 16:20:49 -0700

    Added a script to automatically download (if necessary) and install root from source. Default ROOT version = 5.34.32 (latest v5) - works in 14.04.

---
+++ install_root.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# Script to install root (including pyroot)
+
+INSTALL_DIR=$HOME/InstallationFiles
+# Newest version of ROOT v5. Works in ubuntu 14.04
+ROOT_VERSION=root_v5.34.32.source.tar.gz
+#echo $ROOT_VERSION
+#NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
+
+# Create the installation directory, if it doesn't already exist
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+if [ ! -e $ROOT_VERSION ]; then
+  # Download source tree
+  echo "Downloading source tree..."
+  wget http://root.cern.ch/download/$ROOT_VERSION
+fi
+# Unpack source tree into directory called "root"
+tar -xzf $ROOT_VERSION root
+cd root
+# Configure root (pyroot bindings built by default)
+echo "\n\nConfiguring root..."
+./configure
+# Build
+echo "\n\nBuilding root" # on $(NUM_CPUS) cores"
+make
+# Add root configuration script to bashrc
+echo "Adding thisroot.sh configuration to default environment..."
+echo "source $INSTALL_DIR/root/bin/thisroot.sh" >> $HOME/.bashrc
+echo "ROOT installation complete."
