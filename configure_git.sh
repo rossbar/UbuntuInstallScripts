c839e59eb2a6ededdbac723ce94533e7cb6bf84a
Ross Barnowski <rossbar@berkeley.edu>
2015-09-14 09:29:25 -0700

    Added some git aliases to configure git: unstage and hist.

--- configure_git.sh
+++ configure_git.sh
@@ -5,3 +5,7 @@ git config --global user.email "rossbar@berkeley.edu"
 git config --global credential.helper 'cache --timeout=3600'
 git config --global color.ui true
 git config --global core.editor vim
+
+# Aliases
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.hist 'git log --all --graph --decorate --oneline'
