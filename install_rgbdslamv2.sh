d583e448c44131032deb3039ed1f1efc6ac7b27b
Ross Barnowski <rossbar@berkeley.edu>
2015-12-11 14:56:17 -0800

    updated install_rgbdslamv2 to work with the indigo version

--- install_rgbdslamv2.sh
+++ install_rgbdslamv2.sh
@@ -3,6 +3,7 @@
 # Get ubuntu version
 VERSION_STRING=`lsb_release -r`
 VERSION=${VERSION_STRING//[A-Z:a]/}
+echo "Detected Ubuntu Version: $VERSION"                                        
 
 # Only operate on unbuntu 12.04
 if [ $VERSION == "12.04" ]; then
@@ -23,6 +24,25 @@ if [ $VERSION == "12.04" ]; then
   catkin_make
   catkin_make
   # Add devel script to bashrc
+  echo "source $HOME/rgbdslam_catkin_ws/devel/setup.bash" >> $HOME/.bashrc
+elif [ $VERSION == "14.04" ]; then                                              
+  # Prepare workspace
+  mkdir -p $HOME/rgbdslam_catkin_ws/src
+  cd $HOME/rgbdslam_catkin_ws/src
+  catkin_init_workspace
+  cd $HOME/rgbdslam_catkin_ws
+  catkin_make
+  source devel/setup.bash
+  # Get RGBDSLAM
+  cd $HOME/rgbdslam_catkin_ws/src
+  git clone git@github.com:rossbar/rgbdslam_v2.git
+  cd $HOME/rgbdslam_catkin_ws
+  # Install
+  rosdep update
+  rosdep install rgbdslam
+  catkin_make
+  # Set up environment
+  echo "# ROS environment for RGBDSLAM" >> $HOME/.bashrc
   echo "source $HOME/rgbdslam_catkin_ws/devel/setup.bash" >> $HOME/.bashrc
 
 else
