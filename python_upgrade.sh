220e4f5a709a5bb85e6d0c3e12d0167193a8549c
Ross Barnowski <rossbar@berkeley.edu>
2016-01-25 16:37:17 -0800

    Converted python_upgrade to bash-for to install each package individually

--- python_upgrade.sh
+++ python_upgrade.sh
@@ -5,4 +5,7 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-pip install --upgrade setuptools tornado distribute jinja2 six numpy ipython matplotlib cython scipy pandas scikit-image scikit-learn mayavi ntplib tables pyvtk pyrex numexpr pyopengl rope netifaces pyqtgraph progressbar
+for PACKAGE in setuptools tornado distribute jinja2 six numpy ipython matplotlib cython scipy pandas scikit-image scikit-learn mayavi ntplib tables pyvtk numexpr pyopengl rope netifaces pyqtgraph progressbar
+do
+	pip install --upgrade $PACKAGE
+done
