1ff9ffaf1dc8a464c33708a29873ae06a7516243
Ross Barnowski <rossbar@berkeley.edu>
2016-09-16 11:24:51 -0700

    Added script to install dropbox. Needs testing.

---
+++ install_dropbox.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# NOTE: Removed libboost: libboost1.55-all-dev doesn't play nice with ros-indigo
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+# Get appropriate link for dropbox deb (32 or 64 bit .deb)
+if [ `uname -m` == "x86_64" ]; then
+    echo "Detected 64-bit OS"
+    DB_LINK="https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb";
+elif [ `uname -m` == "i386" ]; then
+    echo "Detected 32-bit OS";
+    DB_LINK="https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_i386.deb";
+else
+    echo "ERROR: Unknown system architecture: `uname -m`"
+    echo "Supported architectures: x86_64, i386"
+    echo "Please install dropbox manually"
+    exit 1;
+fi
+
+# Download
+DL_LOC=$HOME/Downloads
+PKG_NAME="dropbox_$(uname -m).deb"
+mkdir -p $DL_LOC
+cd $DL_LOC
+wget -O $PKG_NAME $DB_LINK 
+
+# Install
+dpkg -i $PKG_NAME
+
+# Run dropbox daemon for initialization
+sudo -u `logname` bash<<___
+  dropbox
+___
+
