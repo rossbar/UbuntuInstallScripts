9169ccbd1bacf0ee2ed49e4738cb25b3d4ffffb9
rossbar <rossbar@berkeley.edu>
2016-10-03 14:45:51 -0700

    Added stress to initial_apt_get.

--- initial_apt_get.sh
+++ initial_apt_get.sh
@@ -7,4 +7,4 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-apt-get install -y build-essential vim libgstreamer-plugins-base0.10-dev libgstreamer-plugins-bad0.10-dev libgstreamer0.10-dev gimp git-core gitk gparted cmake nautilus-open-terminal sqlite3 libhdf5-serial-dev libjpeg62 curl liblapack-dev libatlas-dev vlc python-pip mesa-utils libusb-1.0-0-dev libpng12-dev glmark2 tk-dev tcl tcl8.5-dev openssh-server pigz xclip libfontconfig1-dev libfreetype6-dev libx11-dev libxcursor-dev libxext-dev libxfixes-dev libxft-dev libxi-dev libxrandr-dev libxrender-dev libxpm-dev htop cmake-curses-gui
+apt-get install -y build-essential vim libgstreamer-plugins-base0.10-dev libgstreamer-plugins-bad0.10-dev libgstreamer0.10-dev gimp git-core gitk gparted cmake nautilus-open-terminal sqlite3 libhdf5-serial-dev libjpeg62 curl liblapack-dev libatlas-dev vlc python-pip mesa-utils libusb-1.0-0-dev libpng12-dev glmark2 tk-dev tcl tcl8.5-dev openssh-server pigz xclip libfontconfig1-dev libfreetype6-dev libx11-dev libxcursor-dev libxext-dev libxfixes-dev libxft-dev libxi-dev libxrandr-dev libxrender-dev libxpm-dev htop cmake-curses-gui stress
